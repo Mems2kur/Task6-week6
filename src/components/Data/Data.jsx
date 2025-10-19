c00fd0cb18f433c7ed19edeba9792181b8679d28
Mems_2kur <tukurmaimuna15@gmail.com>
2025-10-19 06:48:25 +0100

    Task 6

--- src/components/Data/Data.jsx
+++ src/components/Data/Data.jsx
@@ -9,22 +9,22 @@ import pizza7 from "../Images/pizza7.jpg";
 import Burger1 from "../Images/Burger1.jpg";
 import Burger2 from "../Images/Burger2.jpg";
 import burger3 from "../Images/burger3.jpg";
-import burger4 from "../Images/burger4.jpg"; // 🍔 burger with fries
-import burger5 from "../Images/burger5.jpg"; // 🍔 burger with fries
-import burger6 from "../Images/burger6.jpg"; // 🍔 new addition
+import Burger4 from "../Images/burger4.jpg"; 
+import Burger5 from "../Images/burger5.jpg";
+import Burger6 from "../Images/burger6.jpg";
 
 export const cards = [
   { id: 1, image: pizza1, title: "Pepperoni Pizza" },
   { id: 2, image: Burger1, title: "Beef Burger Deluxe" },
   { id: 3, image: pizza2, title: "Margherita Pizza" },
-  { id: 4, image: burger4, title: "Double Stack Burger with Fries" },
+  { id: 4, image: Burger4, title: "Double Stack Burger with Fries" },
   { id: 5, image: pizza3, title: "BBQ Chicken Pizza" },
   { id: 6, image: Burger2, title: "Crispy Chicken Burger" },
   { id: 7, image: pizza4, title: "Cheese Burst Pizza" },
   { id: 8, image: burger3, title: "Veggie Cheese Burger" },
   { id: 9, image: pizza5, title: "Veggie Supreme" },
-  { id: 10, image: burger5, title: "Smoky BBQ Burger with Fries" },
+  { id: 10, image: Burger5, title: "Smoky BBQ Burger with Fries" },
   { id: 11, image: pizza6, title: "Spicy Paneer Pizza" },
-  { id: 12, image: burger6, title: "Classic Beef Burger" },
+  { id: 12, image: Burger6, title: "Classic Beef Burger" },
   { id: 13, image: pizza7, title: "Classic Italian Pizza" },
 ];
